390bbdb0ed716d7ed4f653538f7fad293a043e6b
Adam Dymitruk <adam.dymitruk@adaptechgroup.com>
2024-05-08 06:21:22 -0700

    hello world

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  const date = new Date();
+  res.send(`Hello, the current server time is: ${date.toISOString()}`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
