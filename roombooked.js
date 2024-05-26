7efca7ca76f472f7da39c18e751ae2f78284b9f4
Adam Dymitruk <adam@eventsourcing.com>
2024-05-26 12:43:53 -0700

    remove all old files

--- roombooked.js
+++
@@ -1,8 +0,0 @@
-class RoomBooked {
-    constructor(roomId, userId, startTime, endTime) {
-        this.roomId = roomId;
-        this.userId = userId;
-        this.startTime = startTime;
-        this.endTime = endTime;
-    }
-}
