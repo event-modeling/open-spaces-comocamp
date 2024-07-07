97239cebd24aa84df0d0656a9c43183d314c4115
Manuel Ortega <maoc84@gmail.com>
2024-07-07 19:25:59 +0200

    A few code style changes. from multiple lines to one liners

--- ruby/app.rb
+++ ruby/app.rb
@@ -7,40 +7,10 @@ require 'ostruct'
 require_relative 'events/unique_id_provided_event'
 
 EVENT_STORE_PATH = File.expand_path(File.join(__dir__, '..', 'eventstore'))
+def write_event_if_id_not_exists(event) Dir.mkdir(EVENT_STORE_PATH) unless Dir.exist?(EVENT_STORE_PATH); event_files = Dir.entries(EVENT_STORE_PATH).select { |file| file.include?(event.id) }; if event_files.empty? then timestamp = event.timestamp.gsub(':', '-').gsub(/\..+/, ''); file_name = "#{timestamp}-#{event.id}-#{event.type}.json"; File.write(File.join(EVENT_STORE_PATH, file_name), JSON.generate(event.to_h)) end end
+def get_all_events; Dir.mkdir(EVENT_STORE_PATH) unless Dir.exist?(EVENT_STORE_PATH); Dir.entries(EVENT_STORE_PATH).select { |file| file.end_with?('.json') }.map { |file| OpenStruct.new(JSON.parse(File.read(File.join(EVENT_STORE_PATH, file)))) }; end
 
-def write_event_if_id_not_exists(event)
-  Dir.mkdir(EVENT_STORE_PATH) unless Dir.exist?(EVENT_STORE_PATH)
-  event_files = Dir.entries(EVENT_STORE_PATH).select { |file| file.include?(event.id) }
-  if event_files.empty?
-    timestamp = event.timestamp.gsub(':', '-').gsub(/\..+/, '')
-    file_name = "#{timestamp}-#{event.id}-#{event.type}.json"
-    File.write(File.join(EVENT_STORE_PATH, file_name), JSON.generate(event.to_h))
-  end
-end
-
-def get_all_events
-  Dir.mkdir(EVENT_STORE_PATH) unless Dir.exist?(EVENT_STORE_PATH)
-  event_files = Dir.entries(EVENT_STORE_PATH).select { |file| file.end_with?('.json') }
-  event_files.map do |file|
-    file_path = File.join(EVENT_STORE_PATH, file)
-    OpenStruct.new(JSON.parse(File.read(file_path)))
-  end
-end
-
+get '/portal_management' do erb :portal_management, locals: { conference_id: conference_ids_sv(get_all_events).last } end
 def conference_ids_sv(events_array)
-  events_array.select { |event| event.type == 'UniqueIdProvidedEvent' }
-                               .sort_by { |event| event.timestamp }
-                               .reverse
-                               .map do |event|
-    {
-      conf_id: event.confId,
-      qr: event.qr,
-      url: event.url
-    }
-  end
-end
-
-get '/portal_management' do
-  conference_ids = conference_ids_sv(get_all_events)
-  erb :portal_management, locals: { conference_id: conference_ids.last }
+  events_array.select { |event| event.type == 'UniqueIdProvidedEvent' }.sort_by { |event| event.timestamp }.reverse.map { |event| { conf_id: event.confId, qr: event.qr, url: event.url } }
 end
