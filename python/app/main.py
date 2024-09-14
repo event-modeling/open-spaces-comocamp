bd7af64445da68a2b9911eff4d08ed46a98f22b3
James <>
2024-09-14 14:51:17 -0600

    experimenting with topic_voting2

--- python/app/main.py
+++ python/app/main.py
@@ -257,7 +257,10 @@ def open_registration(conference_id: str):
     timestamp = datetime.now().isoformat()
     handler = CommandsHandler()
     handler.open_registration_command(event_id, timestamp, conference_id)
-    return RedirectResponse(url=f'/register/{conference_id}', status_code=status.HTTP_302_FOUND)
+    return {
+        'message': f'Registration opened for conference {conference_id}'
+    }
+
 
 
 # Time Slots
