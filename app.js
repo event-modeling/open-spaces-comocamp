e9ce95d0fb318f75cecc0648cdb15bfc4268de3e
Adam Dymitruk <adam@eventsourcing.com>
2024-11-10 18:07:30 -0800

    first eventstore implementation

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 // app that uses express to show forms and uses musche type of view engine  
 
-const express = require("express");
-const app = express();
+const express = require("express"); const app = express(); const fs = require("fs");
 
-app.set("view engine", "mustache");
-app.engine("mustache", require("mustache-express")());
+(function setupExpressAndMustache() {
+  app.set("view engine", "mustache");
+  app.engine("mustache", require("mustache-express")());
+  app.use(express.static('public'));
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
+})();
 
-app.use(express.static('public'));
+
+function get_events() {
+  return fs.readdirSync("./event-stream").map(file => {
+    return JSON.parse(fs.readFileSync(`./event-stream/${file}`, "utf8"));
+  });
+}
+
+function rooms_state_view() {
+  return get_events().reduce((acc, event) => {
+    if (event.type === "room_added_event") {
+      acc.push(event.room_name);
+    }
+    return acc;
+  }, []);
+}
 
 app.get("/rooms", (req, res) => {
   //render a view of rooms. pass in a collection of rooms
-  res.render("rooms", { rooms: ["room1", "room2", "room3"] });
+  res.render("rooms", { rooms: rooms_state_view() });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
